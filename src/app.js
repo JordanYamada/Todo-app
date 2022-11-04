e53bd25cfc8a3c7966b5620b00d374a34608489f
Jordan Yamada <sadaotomokane@gmail.com>
2022-11-04 07:00:38 +0000

    still working on auth

--- src/app.js
+++ src/app.js
@@ -1,14 +1,16 @@
 import React from 'react';
-
+import LoginProvider from './auth/context.js';
 import ToDo from './components/todo/todo.js';
 import SettingsProvider from './context/settings.js';
 
 export default class App extends React.Component {
   render() {
     return (
-      <SettingsProvider>
-        <ToDo />
-      </SettingsProvider>
+      <LoginProvider>
+        <SettingsProvider>
+          <ToDo />
+        </SettingsProvider>
+      </LoginProvider>
     );
   }
 }
